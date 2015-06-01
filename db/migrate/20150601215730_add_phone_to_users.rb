ad38925dbab6a4572215d7eb2cc2889bfb58f4f3
Darrell Cohn <darrellcohn@gmail.com>
2015-06-01 16:38:51 -0700

    Change phone to string

--- db/migrate/20150601215730_add_phone_to_users.rb
+++ db/migrate/20150601215730_add_phone_to_users.rb
@@ -1,5 +1,5 @@
 class AddPhoneToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :phone, :integer
+    add_column :users, :phone, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150601215730) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "phone"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
