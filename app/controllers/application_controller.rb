632b8dfbec6cffc62c5eec1cf4eef7b6cb525ce1
Darrell Cohn <darrellcohn@gmail.com>
2015-06-01 16:15:39 -0700

    Fix sign up rendering issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation) } 
+	end
+
+
   protect_from_forgery with: :exception
 end
