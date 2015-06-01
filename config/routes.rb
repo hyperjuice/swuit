5d098a90fad2318d254b2e54af71632315a51abe
Darrell Cohn <darrellcohn@gmail.com>
2015-06-01 12:56:53 -0700

    Install Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+    
+
   root 'welcome#index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  
+end
+
+
+
+
+
+
+
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -55,4 +66,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
