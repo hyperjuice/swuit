126befac1287ced9b50aed3dd69ebd7ac7ed12be
Darrell Cohn <darrellcohn@gmail.com>
2015-06-01 19:43:56 -0700

    Redirect user to profile page upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource)
+  	user_path(current_user)
+	end
+
+
 protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :phone, :email, :password, :password_confirmation) } 
