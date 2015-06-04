39965e01254bcf2174ddebafe683d18416dec5da
Darrell Cohn <darrellcohn@gmail.com>
2015-06-03 19:20:32 -0700

    Work on Mapbox integration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
+    @products = Product.all
   end
 
   def new
