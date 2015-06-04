737666488e3af338a87e03f89e6f35efb81120f9
Darrell Cohn <darrellcohn@gmail.com>
2015-06-04 15:14:49 -0700

    Start mapping

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,12 @@
 class MapController < ApplicationController
   def index
   end
+
+  def map
+  	@products = Product.all
+  	respond_to do |format|
+  		format.html
+  		format.json {render json: @products}
+  	
+  end
 end
