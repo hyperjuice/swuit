8f929ebd5da1c1f9dcf9f59b7240ff5ff593c471
Darrell Cohn <darrellcohn@gmail.com>
2015-06-05 09:23:27 -0700

    Fix mapping glitch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,37 +18,3 @@
 //= require moment
 //= require bootstrap-datetimepicker
 //= require pickers
-
-$(function() {
-
-	$.getJSON('/mapdata', function(data) {
-
-		L.mapbox.accessToken = 'pk.eyJ1IjoiaHlwZXJqdWljZSIsImEiOiIwNmNmYjFmYzY2MjIyMjBkYTFjYWJiZWQ3NGZhZjkxMyJ9.nkuZ1eUGDblRS7pm8IAy-A';
-			var map = L.mapbox.map('map', 'hyperjuice.mbd96bic')
-	    .setView([37.865626, -122.280749], 12);
-
-	    var features = [];
-	    for (i =0; i < data.length; i++) {
-
-	    	features.push( {
-
-	    	type: 'Feature',
-	    	geometry: {
-	        type: 'Point',
-	        // coordinates here are in longitude, latitude order because
-	        // x, y is the standard for GeoJSON and many formats
-	        coordinates: [
-	           data[i].longitude, data[i].latitude
-	        ]
-		    },
-		    properties: {
-		        title: ''+data[i].kind,
-		        'marker-size': 'large',
-		        'marker-color': '#000',
-		        'marker-symbol': 'park'
-		    }
-		    	});
-		    }
-		    L.mapbox.featureLayer(features).addTo(map);
-	});
-});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 #Rails.application.config.assets.precompile += %w( map.js )
+
+Rails.application.config.assets.precompile += %w( theMap.js )
