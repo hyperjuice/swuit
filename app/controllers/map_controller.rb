81702d797bf2513eef4307bf9bb999e0c2c1007a
Darrell Cohn <darrellcohn@gmail.com>
2015-06-14 19:20:03 -0700

    Style links for map page

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,12 +2,8 @@ class MapController < ApplicationController
 
 	def index
 	  @products = Product.all
-    # @user = User.find(params[:user_id])
-    # @product = Product.find(params[:id])
 	end
-
-
-
+  
   def map
   	@products = Product.all
   	respond_to do |format|
